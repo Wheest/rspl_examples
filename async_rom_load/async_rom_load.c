8aabdd821669ca1b6c650e74b5fce14c504a2b48
Perry Gibson <perry@gibsonic.org>
2024-04-17 18:55:02 +0200

    Update async load example

--- async_rom_load/async_rom_load.c
+++ async_rom_load/async_rom_load.c
@@ -16,7 +16,7 @@ void do_something() {
   }
 }
 
-void read_serial(unsigned char *numbers) {
+unsigned long read_serial(unsigned char *numbers, int num_values) {
   // Open the file for reading in binary mode
   FILE *fptr = fopen("rom:/integers.dat", "rb");
   if (!fptr) {
@@ -27,8 +27,8 @@ void read_serial(unsigned char *numbers) {
 
   // Read the integers into the array
   unsigned long start = get_ticks();
-  read_count = fread(numbers, sizeof(unsigned char), 1024, fptr);
-  if (read_count != 1024) {
+  read_count = fread(numbers, sizeof(unsigned char), num_values, fptr);
+  if (read_count != num_values) {
     printf("Error reading file\n");
     fclose(fptr);
   }
@@ -40,21 +40,27 @@ void read_serial(unsigned char *numbers) {
 
   unsigned long time_spent_cpu = get_ticks() - start;
   printf("\nCPU baseline time (CPU ticks): %lu\n", time_spent_cpu);
+  /* debug_hexdump(numbers, 1024); */
+  return time_spent_cpu;
 }
 
-void read_async(unsigned char *numbers) {
+unsigned long read_async(unsigned char *numbers, int num_values) {
   // Read the integers into the array
   unsigned long start = get_ticks();
 
-  uint32_t rom_add = dfs_rom_addr("rom:/integers.dat");
+  uint32_t rom_add = dfs_rom_addr("integers.dat");
+  // Need to change initial b with an 1
+  // this might be patched out
+  rom_add = rom_add & 0x1fffffff;
 
-  dma_read_async(numbers, rom_add, 1024);
+  dma_read_async(numbers, rom_add, num_values);
 
   do_something();
   dma_wait();
   unsigned long time_spent_cpu = get_ticks() - start;
 
   printf("\nAsync version time (CPU ticks): %lu\n", time_spent_cpu);
+  return time_spent_cpu;
 }
 
 int main() {
@@ -67,29 +73,30 @@ int main() {
 
   printf("Program starting\n");
 
+  int num_values = 1024;
   unsigned char *numbers;
-  numbers = (void *)memalign(16, ROUND_UP(1024 * sizeof(int), 16));
+  numbers = (void *)memalign(16, ROUND_UP(num_values * sizeof(int), 16));
 
-  read_serial(numbers);
+  unsigned long stime = read_serial(numbers, num_values);
 
   unsigned char *numbers2;
-  numbers2 = (void *)memalign(16, ROUND_UP(1024 * sizeof(int), 16));
+  // numbers2 = (void *)memalign(16, ROUND_UP(1024 * sizeof(int), 16));
+  numbers2 = malloc_uncached_aligned(num_values * sizeof(unsigned char), 16);
 
-  read_async(numbers2);
-
-  for (int i = 0; i < 10; ++i) {
-    printf("%u ", numbers2[i]);
-  }
+  unsigned long atime = read_async(numbers2, num_values);
 
   // check that the data is the same
-  for (int i = 0; i < 1024; ++i) {
+  for (int i = 0; i < num_values; ++i) {
     if (numbers[i] != numbers2[i]) {
       printf("Data mismatch at index %d\n", i);
+      printf("Expected: %u, got: %u\n", numbers[i], numbers2[i]);
+      printf("Peak-ahead values %u, %u\n", numbers[i + 1], numbers2[i + 1]);
       return EXIT_FAILURE;
     }
   }
 
-  printf("Done\n");
+  printf("\nDone and matching!\n");
+  printf("Speedup: %f\n", (float)stime / atime);
 
   return EXIT_SUCCESS;
 }
--- async_rom_load/gen_dat_file.py
+++ async_rom_load/gen_dat_file.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import struct
+import os
+
+# create the filesystem directory if it does not exist
+os.makedirs("filesystem", exist_ok=True)
 
 # Open a file in binary write mode
 with open("filesystem/integers.dat", "wb") as file:
     # Iterate over the range of 1024 8-bit integers
-    for i in range(1024 * 20):
+    for i in range(1024):
         # Ensure the integer is within the 8-bit range
         value = i % 256
         # Pack the integer as an unsigned char ('B') and write to the file
